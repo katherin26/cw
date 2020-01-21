4c08ab265f80d52cde9f23250e996efc622147e5
katherin26 <47015038+katherin26@users.noreply.github.com>
2020-01-21 14:12:48 -0500

    Update codwars1.js

--- codwars1.js
+++ codwars1.js
@@ -2,9 +2,11 @@ const num= [1,-4,7,12];
 sum = 0;
 
   for (let i = 0; i < num.length; i++){
+if (num[i] > 0 ){
   sum = sum + num[i];
-  if (0>+)
-    
+  console.log(sum);
+}
   }
 
-console.log(sum);
+
+  

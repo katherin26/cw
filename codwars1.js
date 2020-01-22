e538af33826c316d259756665c0329da31919a47
Katherin26 <ft.katheochoa@gmail.com>
2020-01-21 20:00:01 -0500

    changing stuff

--- codwars1.js
+++ codwars1.js
@@ -1,10 +1,19 @@
+
+//You get an array of numbers, return the sum of all of the positives ones.
+
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+//Note: if there is nothing to sum, the sum is default to 0.
+
 const num= [1,-4,7,12];
 sum = 0;
 
   for (let i = 0; i < num.length; i++){
+if (num[i] > 0 ){
   sum = sum + num[i];
-  if (0>+)
-    
+  console.log(sum);
+}
   }
 
-console.log(sum);
+
+  //hdghjfvecsefjecsdhvcnhsdvcnvsd
